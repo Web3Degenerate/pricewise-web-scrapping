d23985eacacd83e83e7581eeb4d83c1420e4dcad
web3degenerate <hostnomics@gmail.com>
2024-05-05 20:01:48 -0500

    Updated SearchBar.tsx component to handle Amazon link submission logic

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -7,11 +7,55 @@ const SearchBar = () => {
 
 // Set up useState for searchbar at (58:12): https://youtu.be/lh9XVGv6BHs?si=MCW8VX6Xmw4LKOlQ&t=3492
   const [searchPrompt, setSearchPrompt] = useState('')
+  //Set up is loading state (1:04:16)
+  const [isLoading, setIsLoading] = useState(false)
 
+  //Check amazon link validity (1:01:17)
+  const isValidAmazonProductUrl = (url: string) => {
+    try {
+      const parsedURL = new URL(url)
+      const hostname = parsedURL.hostname
+
+      //Check if hostname conatins amazon.com or other country code
+      if(
+          hostname.includes('amazon.com') || 
+          hostname.includes ('amazon.') || 
+          hostname.endsWith('amaazon')
+        ){
+          return true
+      }
+    }catch (error) {
+      return false
+    }
+
+    //part of hostname if check (1:02:42)
+    return false;
+  }
 
 // (59:20): Since TypeScript define type of event explicitly: https://youtu.be/lh9XVGv6BHs?si=5ZJouZ1hAM4jL_1N&t=3560 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault() //stop page reload
+
+    //check if url entered is valid
+    const isValidLink = isValidAmazonProductUrl(searchPrompt)
+
+    //(1:03:01) - test that our valid link check is working:
+    // alert(isValidLink ? 'Valid Link' : 'Invalid Link')
+
+    //(1:03:48) - exit out if not valid link provided. 
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
+
+      try{
+        setIsLoading(true)
+        // (1:05:28) - finally implement the scrapping logic here. 
+        
+      }catch(error){
+        console.log(error)
+      }finally {
+        // finally block executes either way. (1:04:34): https://youtu.be/lh9XVGv6BHs?si=Ypf_hTyF0xPwUbDf&t=3874
+        setIsLoading(false)
+      }
+
   }
 
   // See (43:13): https://youtu.be/lh9XVGv6BHs?si=VtfrpQypHQ1r9XTC&t=2593
@@ -27,8 +71,12 @@ const SearchBar = () => {
             className="searchbar-input" 
           />
 
-          <button type="submit" className="searchbar-btn">
-
+          <button 
+              type="submit" 
+              className="searchbar-btn"
+              disabled={searchPrompt === ''}
+          >
+            {isLoading ? 'Searching...' : 'Search'}
           </button>
 
     </form>
