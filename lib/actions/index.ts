6e1e19af9e7c28e4ed4729918ef03c30ea403b9e
hostnomics <hostnomics@gmail.com>
2024-05-06 15:19:15 -0600

    Added lib/scrapper/index.ts to handle the web scrapping logic

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,15 @@
 "use server"
 
+import { scrapeAmazonProduct } from "../scrapper";
+
 // Set up at (1:06:55): https://youtu.be/lh9XVGv6BHs?si=eObVhgh8LZ4enCMa&t=4015
 
 export async function scrapeAndStoreProduct (productUrl: string) {
+  if(!productUrl) return
 
+  try{
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+  }catch(error: any){
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
 }
---
+++ lib/scrapper/index.ts
@@ -0,0 +1,5 @@
+// Set up at (1:08:35): https://youtu.be/lh9XVGv6BHs?si=303L2EDVo2GMf8HN&t=4115
+
+export async function scrapeAmazonProduct(url: string){
+  
+}
